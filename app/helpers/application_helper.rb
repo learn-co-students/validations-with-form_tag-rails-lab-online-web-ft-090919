9452059c2ca6ffd57e08a7e78e3c4089b91f827d
Doug Kintop <dkintop@hotmail.com>
2019-11-11 15:03:14 -0500

    Done.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+    def conditional_tag(name, condition, options=nil, &block)
+  if condition
+    content_tag name, capture(&block), options
+  else
+    capture(&block)
+  end
+ end
+
 end
